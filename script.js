04c9973308177217c7f4ad4628a38d1fb4668ffe
Doug Hammer <doug.hammer16@gmail.com>
2021-05-24 16:08:29 -0700

    Boiler plate html/operator functions

---
+++ script.js
@@ -0,0 +1,21 @@
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+const operate = (num1, num2, operator) => {
+  switch(operator) {
+    case '+':
+      return add(num1, num2);
+      break;
+    case '-':
+      return subtract(num1, num2);
+      break;
+    case '*':
+      return multiply(num1, num2);
+      break;
+    case '/':
+      return divide(num1, num2);
+      break;
+  };
+}
