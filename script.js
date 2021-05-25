17df7080c973baa3616fef091423af7a6a2efa3f
Doug Hammer <doug.hammer16@gmail.com>
2021-05-25 14:39:14 -0700

    event listeners, screen content updates when number is clicked, clear button functioning

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const screen = document.querySelector('.screen');
+const clearBtn = document.querySelector('button[data-clear]');
+const numBtns = document.querySelectorAll('button[data-number]');
+const operatorBtns = document.querySelectorAll('button[data-operator]');
+let btns = document.querySelectorAll('.btn');
+let num1 = 0;
+let num2 = 0;
+
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
@@ -19,3 +27,22 @@ const operate = (num1, num2, operator) => {
       break;
   };
 }
+
+btns.forEach(button => {
+  button.addEventListener('click', e => {
+    if(button.dataset.operator) {
+      console.log(button)
+    }
+  });
+});
+
+numBtns.forEach(button => {
+  button.addEventListener('click', e => {
+    screen.textContent += button.textContent;
+  });
+});
+
+clearBtn.addEventListener('click', e => {
+  screen.textContent = '';
+});
+
