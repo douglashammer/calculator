ed602cf16011038b88e78f62a3e2c1718064cf09
Doug Hammer <doug.hammer16@gmail.com>
2021-05-25 19:57:58 -0700

    Start of logic w/ event listeners

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const clearBtn = document.querySelector('button[data-clear]');
 const numBtns = document.querySelectorAll('button[data-number]');
 const operatorBtns = document.querySelectorAll('button[data-operator]');
 let btns = document.querySelectorAll('.btn');
-let num1 = 0;
-let num2 = 0;
+let num1 = '';
+let num2 = '';
 
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
@@ -12,37 +12,53 @@ const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
 const operate = (num1, num2, operator) => {
-  switch(operator) {
-    case '+':
-      return add(num1, num2);
-      break;
-    case '-':
-      return subtract(num1, num2);
-      break;
-    case '*':
-      return multiply(num1, num2);
-      break;
-    case '/':
-      return divide(num1, num2);
-      break;
-  };
-}
+	switch (operator) {
+		case '+':
+			return add(num1, num2);
+			break;
+		case '-':
+			return subtract(num1, num2);
+			break;
+		case '*':
+			return multiply(num1, num2);
+			break;
+		case '/':
+			return divide(num1, num2);
+			break;
+	}
+};
 
-btns.forEach(button => {
-  button.addEventListener('click', e => {
-    if(button.dataset.operator) {
-      console.log(button)
-    }
-  });
+let numberClicked = false;
+let operatorClicked = false;
+
+btns.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		if (button.dataset.number) {
+			numberClicked = true;
+			screen.textContent += e.target.textContent;
+			num1 += button.dataset.number;
+		}
+		if (button.dataset.operator) {
+			if (numberClicked) {
+				operatorClicked = true;
+			}
+		}
+		if (button.dataset.number) {
+			if (operatorClicked) {
+				console.log(operatorClicked);
+			}
+		}
+	});
 });
 
-numBtns.forEach(button => {
-  button.addEventListener('click', e => {
-    screen.textContent += button.textContent;
-  });
+numBtns.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		//screen.textContent += button.textContent;
+	});
 });
 
-clearBtn.addEventListener('click', e => {
-  screen.textContent = '';
+clearBtn.addEventListener('click', (e) => {
+	screen.textContent = '';
+	num1 = '';
+	num2 = '';
 });
-
